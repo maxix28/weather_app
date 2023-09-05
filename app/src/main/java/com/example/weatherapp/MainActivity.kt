fbf7db9a1cc43612d293721180b95baf5f92ec07
MaxPavliv <maxyxpavliv@gmail.com>
2023-09-05 19:01:37 +0300

    find your location

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.example.weatherapp
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.adapter.RvAdapter
@@ -11,6 +21,9 @@ import com.example.weatherapp.data.forecastModels.ForecastData
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.databinding.FiveDayBottomBinding
 import com.example.weatherapp.utils.RetrofitInstance
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
@@ -22,13 +35,13 @@ import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-
+import android.provider.Settings
 class MainActivity : AppCompatActivity() {
     lateinit var  binding : ActivityMainBinding
     lateinit var  forecast_binding : FiveDayBottomBinding
 private lateinit var  dialog: BottomSheetDialog
      var  city = "lviv"
-
+private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,7 +51,7 @@ private lateinit var  dialog: BottomSheetDialog
         forecast_binding= FiveDayBottomBinding.inflate(layoutInflater)
         dialog.setContentView(forecast_binding.root)
 
-
+fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
 
 
       binding.searchView11.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener{
@@ -56,6 +69,8 @@ private lateinit var  dialog: BottomSheetDialog
 
       })
 
+        //fetchLocation()
+        fetchLocation()
 
         getCurrentWeather(city)
 
@@ -63,6 +78,69 @@ private lateinit var  dialog: BottomSheetDialog
 
             openDialog()
 
+        }
+
+
+
+        binding.tvLocation.setOnClickListener {
+            fetchLocation()
+
+        }
+    }
+    fun requestLocationServicesActivation(context: Context) {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        context.startActivity(intent)
+    }
+    fun areLocationServicesEnabled(context: Context): Boolean {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun fetchLocation() {
+
+
+        if (!areLocationServicesEnabled(this)) {
+            // Location services are disabled, request activation.
+            requestLocationServicesActivation(this)
+            return
+        }
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),101
+            )
+            return
+        }
+
+        task.addOnSuccessListener {
+            val geocoder=Geocoder(this,Locale.getDefault())
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                geocoder.getFromLocation(it.latitude,it.longitude,1, object: Geocoder.GeocodeListener{
+                    override fun onGeocode(addresses: MutableList<Address>) {
+                        city = addresses[0].locality
+                    }
+
+                })
+            }else{
+                val address = geocoder.getFromLocation(it.latitude,it.longitude,1) as List<Address>
+
+                city = address[0].locality
+            }
+
+            getCurrentWeather(city)
         }
     }
 
